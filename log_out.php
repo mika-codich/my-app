19081b36bcfb2002064ffa035c5b685b2c7c9c3a
BeInAction <91090502+BeInAction@users.noreply.github.com>
2022-12-06 10:27:55 +0300

    Delete log_out.php

--- log_out.php
+++
@@ -1,7 +0,0 @@
-<?php
-setcookie("this_user", $email_modal, time() - 3600);
-$page = $_POST['the_page'];
-//echo "<script>window.location.href = '$this_page'</script>";
-header("Location: $page");
-echo "<script>alert($page)</script>";
-?>
